300c1970fcd8d22cdb9854ad4965204297b4d3d7
georgynejmeh <georgynejmeh.job@gmail.com>
2024-11-22 23:59:23 +0300

    admin: users delete selected

--- src/pages/admin/AdminUsersTable.tsx
+++ src/pages/admin/AdminUsersTable.tsx
@@ -8,6 +8,7 @@ import useGetData from "../../hooks/useGetData";
 import useDeleteData from "../../hooks/useDeleteData";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import useDeleteMultiple from "../../hooks/useDeleteMultiple";
 
 const AdminUsersTable = () => {
   const { isLoading, error, data } = useGetData<{ data: User[] }>("users");
@@ -39,7 +40,6 @@ const AdminUsersTable = () => {
       setItemToDelete(null);
     }
   };
-
   useEffect(() => {
     if (dataDelete) {
       toast.success(`deleted successfully!`);
@@ -48,6 +48,47 @@ const AdminUsersTable = () => {
       toast.error(`Failed to delete. Please try again.`);
     }
   }, [dataDelete, errorDelete]);
+
+  /* DELETE SELECTED */
+  const [selectedUserIds, setSelectedUserIds] = useState<string[]>([]);
+  const handleCheckboxChange = (id: string) => {
+    setSelectedUserIds((prevSelected) => {
+      if (prevSelected.includes(id)) {
+        return prevSelected.filter((userId) => userId !== id);
+      } else {
+        return [...prevSelected, id];
+      }
+    });
+  };
+  const handleUnselectAll = () => {
+    setSelectedUserIds([]);
+  };
+  /* API DELETE SELECTED */
+  const {
+    deleteMultiple,
+    isLoading: isLoadingDeleteMultiple,
+    error: errorDeleteMultiple,
+    data: dataDeleteMultiple,
+  } = useDeleteMultiple("users");
+  const [isDeleteMultipleModalOpen, setIsDeleteMultipleModalOpen] =
+    useState(false);
+  const handleConfirmDeleteMultiple = async () => {
+    try {
+      await deleteMultiple({ userIds: selectedUserIds });
+      setIsDeleteMultipleModalOpen(false);
+      setSelectedUserIds([]);
+    } catch (error) {
+      setIsDeleteModalOpen(false);
+    }
+  };
+  useEffect(() => {
+    if (dataDeleteMultiple) {
+      toast.success(`deleted successfully!`);
+    }
+    if (errorDeleteMultiple) {
+      toast.error(`Failed to delete. Please try again.`);
+    }
+  }, [dataDeleteMultiple, errorDeleteMultiple]);
 
   return (
     <>
@@ -61,12 +102,43 @@ const AdminUsersTable = () => {
         isLoading={isLoadingDelete}
       />
 
+      <DeleteConfirmDialog
+        isOpen={isDeleteMultipleModalOpen}
+        onCancel={() => setIsDeleteMultipleModalOpen(false)}
+        onConfirm={handleConfirmDeleteMultiple}
+        details={`${selectedUserIds.length} users`}
+        isLoading={isLoadingDeleteMultiple}
+      />
+
       <Link to={"/admin/users/new"}>
         <FloatingActionButtonAdd />
       </Link>
 
-      <main className="p-page">
+      <main className="p-page flex flex-col">
         <div>UsersTable</div>
+        {selectedUserIds.length !== 0 && (
+          <div className="flex gap-4 self-end">
+            <button
+              onClick={() => setIsDeleteMultipleModalOpen(true)}
+              className="rounded px-4 py-1 bg-red-800"
+            >
+              <div className="flex items-center gap-2">
+                <FaTrash color="white" />
+                <span className="text-white">
+                  {selectedUserIds.length} selected
+                </span>
+              </div>
+            </button>
+            <button
+              onClick={handleUnselectAll}
+              className="border border-black px-3 py-1 rounded"
+            >
+              <div className="flex items-center gap-2">
+                <span>unselect all</span>
+              </div>
+            </button>
+          </div>
+        )}
         {isLoading ? (
           <span>Loading...</span>
         ) : error ? (
@@ -89,7 +161,11 @@ const AdminUsersTable = () => {
               {data.data.map((user) => (
                 <tr key={user.id}>
                   <td>
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      checked={selectedUserIds.includes(user.id)}
+                      onChange={() => handleCheckboxChange(user.id)}
+                    />
                   </td>
                   <td>{user.id}</td>
                   <td>{user.name}</td>
