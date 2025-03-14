984f50fa4ec20383f385fe2ac03862ce1d4f0890
joellemahfoud9 <joellemahfoud99@gmail.com>
2025-03-14 10:46:15 +0200

    photos

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -4,21 +4,22 @@ const Banner = () => {
       <div className="grid grid-cols-3 gap-4">
         <div className="w-full">
           <img
-            src="src/assets/Banner/Wedding-Traditions-Bouquet-5.jpg"
+            src="\public\Banner\personalised-wedding-invitation-thin-watercolour-wreath-portrait-a5-flat-1-blue.jpg"
             alt="Image 1"
             className="w-full h-[100%]  object-cover"
           />
         </div>
         <div className="w-full">
           <img
-            src="src/assets/Banner/20200514033505_file_5ebd65299a07c_5ebd659deb0b1.avif"
+
             alt="Image 2"
+            src="\public\Banner\20200514033505_file_5ebd65299a07c_5ebd659deb0b1.avif"
             className="w-full h-[100%] object-cover"
           />
         </div>
         <div className="w-full">
           <img
-            src="src/assets/Banner/personalised-wedding-invitation-thin-watercolour-wreath-portrait-a5-flat-1-blue.jpg"
+            src="\public\Banner\Wedding-Traditions-Bouquet-5.jpg"
             alt="Image 3"
             className="w-full h-[100%]  object-cover"
           />
