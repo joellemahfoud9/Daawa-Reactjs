25f08648406192ab6e985876402dabe2063843b1
joellemahfoud9 <joellemahfoud99@gmail.com>
2025-03-24 08:33:25 +0200

    Dashboard

--- src/components/home/Testimonial.tsx
+++ src/components/home/Testimonial.tsx
@@ -1,7 +1,7 @@
-import { useTranslation } from 'react-i18next';
+
 
 const Testimonial = () => {
-  const { t } = useTranslation();
+  
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-center mt-10 pt-12 w-full">
